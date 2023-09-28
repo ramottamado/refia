c2ba702a6dad1705ac142ad1b20eec061fde1624
Tamado Ramot Sitohang <ramot@ramottamado.dev>
2023-09-29 00:29:40 +0700

    Add OneToMany relationship to Publisher and Category entities

--- src/main/java/codes/tamado/refia/entity/Book.java
+++ src/main/java/codes/tamado/refia/entity/Book.java
@@ -66,7 +66,7 @@ public class Book {
     return code;
   }
 
-  public void setCode(String code) {
+  public void setCode(final String code) {
     this.code = code;
   }
 
--- src/main/java/codes/tamado/refia/entity/Publisher.java
+++ src/main/java/codes/tamado/refia/entity/Publisher.java
@@ -1,10 +1,15 @@
 package codes.tamado.refia.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
@@ -21,6 +26,12 @@ public class Publisher {
   @JdbcTypeCode(SqlTypes.VARCHAR)
   private String name;
 
+  @OneToMany(mappedBy = "publisher", cascade = {
+      CascadeType.PERSIST,
+      CascadeType.MERGE
+  })
+  private Set<Book> books = new LinkedHashSet<>();
+
   public Long getId() {
     return id;
   }
@@ -31,5 +42,13 @@ public class Publisher {
 
   public void setName(final String name) {
     this.name = name;
+  }
+
+  public Set<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(Set<Book> books) {
+    this.books = books;
   }
 }
